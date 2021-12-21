06cad29080e1c4cd4c7e059178d54a62a5480d04
jarek <js100code@gmail.com>
2021-12-21 11:43:58 +0100

    Added class Topic and Test

--- web/app.py
+++ web/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 # from pymongo import MongoClient
-from functions import verify_user, hash_password
+from functions import hash_password
 from flask_mongoengine import MongoEngine
-
+from datetime import datetime
 
 
 
@@ -22,13 +22,27 @@ db.init_app(app)
 
 
 class User(db.Document):
-    name = db.StringField()
+    name = db.StringField(unique=True)
     password = db.StringField()
-    role = db.StringField()
- 
+    admin = db.BooleanField()
+    test_creator = db.BooleanField()
+    date_of_registration = db.DateTimeField(default=datetime.utcnow)
 
 
 
+
+class Topic(db.EmbeddedDocumentt):
+    topic_name = StringField(required=True)
+    philosopher = StringField(required=True)
+    contents = StringField(required=True)
+
+
+
+class Test(db.Document):
+    author = ReferenceField(User)
+    test_name = StringField(required=True)
+    topics = db.EmbeddedDocumentListField(Topic)
+    
 
 # connector = MongoClient("mongodb://db:27017")
 # db = connector.SokratesDB
@@ -43,9 +57,8 @@ class Register(Resource):
         name = postedData["user_name"]
         password = postedData["password"]
         hashed_pw = hash_password(password) 
-        role = postedData["role"]
-
-        user = User(name=name, password=hashed_pw, role = role)
+       
+        user = User(name=name, password=hashed_pw, admin=False, test_creator=False )
         user.save()
 
         return jsonify({
@@ -53,62 +66,62 @@ class Register(Resource):
             "msg" : "Registration - done",
             "name" :user.name,
             "password": user.password,
-            "role" :role
+            "admin" :user.admin,
+            "test_creator" : user.test_creator,
+            "date_of_regist": user.date_of_registration
         })
 
 
 
-# class AdminPanel(Resource):
-#     def post(self):
-#         postedData = request.getjson()
+class AdminPanel(Resource):
+    def post(self):
+        postedData = request.get_json()
+       
+        #Login as an admin
+        name = postedData["user_name"]
+        password = postedData["password"]
+        hashed_pw = hash_password(password)
 
-#         user_name = postedData["user_name"]
-#         password = postedData["password"]
+        user = User.objects(name=name).get()
+        if not user:
+            return jsonify({
+                "status":404,
+                "msg": "user not found"
+            })
+
+        if not user.admin:
+            return jsonify({
+                "status": 301,
+                "msg": "user is not admin"
+            }) 
+
+        
+        if postedData['delete_user']:
+            user_to_delete=User.objects(name=postedData["user_to_delete"])
+            user_to_delete.delete()
+
+            return jsonify({
+                "status": 200,
+                "msg": "user is deleted "
+            })
+
+
+class TestCreator(Resource):
+    def post(self):
+        postedData = request.get_json()
+       
+        #Login as an admin
+        name = postedData["user_name"]
+        test_name = postedData["test_name"]
         
 
-#         isAdmin = verify_user(user_name, password, db, check_role=True)
-#         if not isAdmin:
-#             return jsonify({
-#                 "status" :302,
-#                 "msg" : "Incorrect password or user name"
-#             })
-
-#         return jsonify({
-#             "msg" : "You are admin"
-#         })
-
-
-
-
-
-class Test(Resource):
-    def get(self):
-        # db.users.drop()
-        # dbs_list = connector.list_database_names()
-        # cols_list = db.list_collection_names()
-        # users_names = []
-        # for user in users.find({}, {"_id": 0, "user_name": 1, "role": 1}):
-        #     users_names.append(user)
-        # users_names = str(users_names)
-        
-        # users_list = []
-        # for u in User.objects():
-        #     users_list.append(u)
-        user = User.objects(name='Tess').first()
-
-
-        return jsonify( {
-            "msg": "test web Sokrates_app. OK!",
-            "name": user.name,
-            "pasword": user.password,
-            "role": user.role
-
-        } )
+class AddTopic(Resource):
+    pass
 
 
 api.add_resource(Test, '/test')
 api.add_resource(Register, '/register')
-# api.add_resource(AdminPanel, '/adminpanel')
+api.add_resource(AdminPanel, '/adminpanel')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
