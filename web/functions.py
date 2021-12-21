fa6b98bc990a40fb170e743ee27e4bf7cd0a12b0
jarek <js100code@gmail.com>
2021-12-21 11:44:42 +0100

    Started build schema for tests

--- web/functions.py
+++ web/functions.py
@@ -3,17 +3,4 @@ import bcrypt
 def hash_password(password):
     return bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
 
-def verify_user(username, password, db, check_role=False):
-    pass
-    # if not check_role:
-    #     hashed_pw = db.find({
-    #         "Username":username
-    #     })[0]["password"]
 
-    #     if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
-    #         return True
-    #     else:
-    #         return False
-    # else:
-
-    # True if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed else False
